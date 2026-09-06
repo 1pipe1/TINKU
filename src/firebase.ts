776e0ce52b81dd1283fe06f34ffebacd3f25bdb6
1pipe1 <beltranfelipe579@gmail.com>
2026-09-06 16:57:55 -0500

    feat: agregar emuladores de Firestore y Auth en la configuración de Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};  
+};
 
 // Initialize Firebase
 
@@ -17,3 +17,12 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+// 🔌 Comenta esto para volver a la base de datos real de internet:
+/*
+if (import.meta.env.DEV) {
+  console.log("🔌 Conectando a los Emuladores...");
+  connectFirestoreEmulator(db, "localhost", 8080);
+  connectAuthEmulator(auth, "http://localhost:9099");
+}
+*/
